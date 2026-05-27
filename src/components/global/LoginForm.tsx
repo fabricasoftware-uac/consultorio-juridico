40ae47c82e3fff6a87d5e09036ef92974a2a69b6
salomontillaUni <thebilystaxx@gmail.com>
2026-05-26 20:13:31 -0500

    agrega console log

--- src/components/global/LoginForm.tsx
+++ src/components/global/LoginForm.tsx
@@ -56,6 +56,7 @@ export function LoginForm() {
       if (session) {
         const jwt = jwtDecode<CustomJwtPayload>(session.access_token);
         const role = jwt.user_role;
+        console.log("Rol del usuario:", role);
         switch (role) {
           case "admin":
             router.refresh();
