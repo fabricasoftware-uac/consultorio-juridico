aa0cb7c955ed29a043d52137e4e91ea4827ee722
salomontillaUni <thebilystaxx@gmail.com>
2026-07-16 17:59:35 -0500

    corrige formatter de fecha

--- src/lib/format-date.ts
+++ src/lib/format-date.ts
@@ -1,5 +1,7 @@
 export const formatDate = (dateString: string) => {
-  return new Date(dateString).toLocaleDateString("es-ES", {
+  const dateOnly = dateString.split("T")[0]; // "2026-07-14"
+  const [year, month, day] = dateOnly.split("-").map(Number);
+  return new Date(year, month - 1, day).toLocaleDateString("es-ES", {
     year: "numeric",
     month: "long",
     day: "numeric",
